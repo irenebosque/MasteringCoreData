b9994231845be7542809a01d6286ef2fe158339b
irenebosque <ireneboske@gmail.com>
2023-01-10 19:05:50 +0100

    Initial Commit

---
+++ MasteringCoreData/MasteringCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasteringCoreDataApp.swift
+//  MasteringCoreData
+//
+//  Created by Irene on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MasteringCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
